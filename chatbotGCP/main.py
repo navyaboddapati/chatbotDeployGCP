7111f62188f1da5478aef2bebd3de1bfebbfe904
navyaboddapati <72976632+navyaboddapati@users.noreply.github.com>
2024-05-06 04:30:01 -0500

    main files for gcp

--- chatbotGCP/main.py
+++ chatbotGCP/main.py
@@ -1,167 +1,154 @@
 import re
-import sqlite3
 from flask import Flask, request, jsonify, render_template
+from google.cloud import firestore
 
 app = Flask(__name__)
+
+# Initialize Firestore client
+db = firestore.Client()
 
 # Define functions for dynamic responses
 
 def get_books_response(search_term=None):
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-
+    books_collection = db.collection('BookCollection')
     if search_term:
-        c.execute("SELECT * FROM BookCollection WHERE LOWER(title) LIKE ? OR identifier1 LIKE ? OR identifier2 LIKE ?",
-                  ('%' + search_term.lower() + '%', '%' + search_term + '%', '%' + search_term + '%'))
-        books = c.fetchall()
+        query = books_collection.where('title', '>=', search_term.lower()).where('title', '<=', search_term.lower() + '\uf8ff')
+        books = query.stream()
     else:
-        c.execute("SELECT * FROM BookCollection")
-        books = c.fetchall()
-
-    conn.close()
-
-    if books:
-        book_info = ""
-        for book in books[:5]:  # Limit to 5 books
-            book_info += f"<b>Title: {book[1]}</b><br>"
-            book_info += f"Publisher: {book[2]}<br>"
-            if book[3]:
-                book_info += f"{book[3]}<br>"
-                book_info += f"{book[4]}<br>"
-            book_info += f"Link: <a href='{book[5]}'>{book[5]}</a><br><br>"
+        books = books_collection.stream()
+    
+    book_info = ""
+    for book in books:
+        book_data = book.to_dict()
+        book_info += f"<b>Title: {book_data.get('title', '')}</b><br>"
+        book_info += f"Publisher: {book_data.get('publisher', '')}<br>"
+        if book_data.get('identifier1'):
+            book_info += f"{book_data.get('identifier1', '')}<br>"
+        if book_data.get('identifier2'):
+            book_info += f"{book_data.get('identifier2', '')}<br>"
+        book_info += f"Link: <a href='{book_data.get('link', '')}'>{book_data.get('link', '')}</a><br><br>"
+    
+    if book_info:
         return book_info
     else:
         return "No books found in the library database."
 
 def get_bookt_response(search_term):
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-
-    if search_term:
-        # Use a parameterized query to avoid SQL injection
-        c.execute("SELECT * FROM BookCollection WHERE LOWER(title) LIKE ?", ('%' + search_term.lower() + '%',))
-        books = c.fetchall()
-    else:
-        c.execute("SELECT * FROM BookCollection")
-        books = c.fetchall()
-
-    conn.close()
-
-    if books:
-        book_info = ""
-        for book in books:
-            book_info += f"<b>Title: {book[1]}</b><br>"
-            book_info += f"Publisher: {book[2]}<br>"
-            if book[3]:
-                book_info += f"{book[3]}<br>"
-                book_info += f"{book[4]}<br>"
-            book_info += f"Link: <a href='{book[5]}'>{book[5]}</a><br><br>"
+    books_collection = db.collection('BookCollection')
+    query = books_collection.where('title', '>=', search_term.lower()).where('title', '<=', search_term.lower() + '\uf8ff')
+    books = query.stream()
+    
+    book_info = ""
+    for book in books:
+        book_data = book.to_dict()
+        book_info += f"<b>Title: {book_data.get('title', '')}</b><br>"
+        book_info += f"Publisher: {book_data.get('publisher', '')}<br>"
+        if book_data.get('identifier1'):
+            book_info += f"{book_data.get('identifier1', '')}<br>"
+        if book_data.get('identifier2'):
+            book_info += f"{book_data.get('identifier2', '')}<br>"
+        book_info += f"Link: <a href='{book_data.get('link', '')}'>{book_data.get('link', '')}</a><br><br>"
+    
+    if book_info:
         return book_info
     else:
         return "No books found in the library database."
 
 def get_events_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM NewsEventCollection")
-    events = c.fetchall()
-    conn.close()
-
-    if events:
-        event_info = "<b>Here are the upcoming events:</b><br>"
-        for event in events[:5]:  # Limit to 5 events
-            event_info += f"<b>{event[2]} on {event[1]}</b><br>"
-            event_info += f"Link: <a href='{event[3]}'>{event[3]}</a><br>"
+    events_collection = db.collection('NewsEventCollection')
+    events = events_collection.stream()
+    
+    event_info = "<b>Here are the upcoming events:</b><br>"
+    for event in events:
+        event_data = event.to_dict()
+        event_info += f"<b>{event_data.get('title', '')} on {event_data.get('date', '')}</b><br>"
+        event_info += f"Link: <a href='{event_data.get('link', '')}'>{event_data.get('link', '')}</a><br>"
+    
+    if event_info:
         return event_info
     else:
         return "There are no upcoming events at the moment."
 
 def get_locations_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM LocationCollection")
-    locations = c.fetchall()
-    conn.close()
-
-    if locations:
-        location_info = "<b>Here are the library locations:</b><br>"
-        for location in locations:
-            location_info += f"<b>{location[1]}</b><br>"
-            location_info += f"Link: <a href='{location[2]}'>{location[2]}</a><br>"
+    locations_collection = db.collection('LocationCollection')
+    locations = locations_collection.stream()
+    
+    location_info = "<b>Here are the library locations:</b><br>"
+    for location in locations:
+        location_data = location.to_dict()
+        location_info += f"<b>{location_data.get('library', '')}</b><br>"
+        location_info += f"Link: <a href='{location_data.get('link', '')}'>{location_data.get('link', '')}</a><br>"
+    
+    if location_info:
         return location_info
     else:
         return "No library locations found in the database."
 
 def get_central_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM LocationCollection WHERE LOWER(library) LIKE '%central%'")
-    central_location = c.fetchone()  # Fetch only one record
-    conn.close()
-
+    locations_collection = db.collection('LocationCollection')
+    query = locations_collection.where('library', '>=', 'central').where('library', '<=', 'central\uf8ff')
+    central_location = next(query.stream(), None)
+    
     if central_location:
+        location_data = central_location.to_dict()
         location_info = "<b>Central Library</b><br>"
-        location_info += f"Link: <a href='{central_location[2]}'>{central_location[2]}</a><br>"
+        location_info += f"Link: <a href='{location_data.get('link', '')}'>{location_data.get('link', '')}</a><br>"
         return location_info
     else:
         return "Central library location not found in the database."
-    
+
 def get_west_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM LocationCollection WHERE LOWER(library) LIKE '%west%'")
-    central_location = c.fetchone()  # Fetch only one record
-    conn.close()
-
-    if central_location:
-        location_info = "<b>West Campus Library</b><br>"
-        location_info += f"Link: <a href='{central_location[2]}'>{central_location[2]}</a><br>"
-        return location_info
-    else:
-        return "Central library location not found in the database."
+    locations_collection = db.collection('LocationCollection')
+    query = locations_collection.where('library', '>=', 'west').where('library', '<=', 'west\uf8ff')
+    west_location = next(query.stream(), None)
     
-def get_science_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM LocationCollection WHERE LOWER(library) LIKE '%science%'")
-    central_location = c.fetchone()  # Fetch only one record
-    conn.close()
-
-    if central_location:
-        location_info = "<b>Science and Engineering Library</b><br>"
-        location_info += f"Link: <a href='{central_location[2]}'>{central_location[2]}</a><br>"
+    if west_location:
+        location_data = west_location.to_dict()
+        location_info = "<b>West Campus Library</b><br>"
+        location_info += f"Link: <a href='{location_data.get('link', '')}'>{location_data.get('link', '')}</a><br>"
         return location_info
     else:
-        return "Central library location not found in the database."
+        return "West Campus library location not found in the database."
+
+def get_science_response():
+    locations_collection = db.collection('LocationCollection')
+    query = locations_collection.where('library', '>=', 'science').where('library', '<=', 'science\uf8ff')
+    science_location = next(query.stream(), None)
+    
+    if science_location:
+        location_data = science_location.to_dict()
+        location_info = "<b>Science and Engineering Library</b><br>"
+        location_info += f"Link: <a href='{location_data.get('link', '')}'>{location_data.get('link', '')}</a><br>"
+        return location_info
+    else:
+        return "Science and Engineering library location not found in the database."
 
 def get_research_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM ResearchCollection")
-    research_items = c.fetchall()
-    conn.close()
-
-    if research_items:
-        research_info = "<b>Here are some research resources:</b><br>"
-        for item in research_items[:5]:  # Limit to 5 items
-            research_info += f"<b>{item[1]}</b><br>"
-            research_info += f"Link: <a href='{item[2]}'>{item[2]}</a><br>"
+    research_collection = db.collection('ResearchCollection')
+    research_items = research_collection.stream()
+    
+    research_info = "<b>Here are some research resources:</b><br>"
+    for item in research_items:
+        item_data = item.to_dict()
+        research_info += f"<b>{item_data.get('title', '')}</b><br>"
+        research_info += f"Link: <a href='{item_data.get('link', '')}'>{item_data.get('link', '')}</a><br>"
+    
+    if research_info:
         return research_info
     else:
         return "No research resources found in the database."
 
 def get_ask_response():
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM AskCollection")
-    ask_items = c.fetchall()
-    conn.close()
-
-    if ask_items:
-        ask_info = "<b>Here are some frequently asked questions:</b><br>"
-        for item in ask_items[:5]:  # Limit to 5 items
-            ask_info += f"<b>{item[1]}</b><br>"
-            ask_info += f"{item[3]}<br><br>"  # Assuming item[2] contains HTML formatted answer
+    ask_collection = db.collection('AskCollection')
+    ask_items = ask_collection.stream()
+    
+    ask_info = "<b>Here are some frequently asked questions:</b><br>"
+    for item in ask_items:
+        item_data = item.to_dict()
+        ask_info += f"<b>{item_data.get('question', '')}</b><br>"
+        ask_info += f"{item_data.get('answer', '')}<br><br>"
+    
+    if ask_info:
         return ask_info
     else:
         return "No frequently asked questions found in the database."
@@ -170,7 +157,7 @@ def get_ask_response():
 
 responses = {
     r'hi|hello': 'Hello!',
-    r'how are you?|howdy?|hi, how are you?': 'I\'m doing well, thank you!',
+    r'how are you?': 'I\'m doing well, thank you!',
     r'who are you?': 'I\'m UTA Library Chatbot.',
     r'what is your name?': 'My name is UTA Library Chatbot.',
     r'events?': get_events_response,
@@ -182,27 +169,26 @@ responses = {
     r'science and engineering library|science & engineering library|science library|engineering library|science engineering|science engineering library': get_science_response,
     r'research': get_research_response,
     r'ask?': get_ask_response,
-    r'bye|quit|exit': 'Goodbye!'
+    r'quit|exit': 'Goodbye!'
 }
 
 # Define main response handling function
+
 def get_response(user_input):
     user_input = user_input.lower()
-    conn = sqlite3.connect('library.db')
-    c = conn.cursor()
 
     for pattern, response in responses.items():
         if re.search(pattern, user_input):
             if callable(response):  # Check if the response is a function
                 return response()  # If it is, call the function and return its result
             return response  # Otherwise, return the response string
-    
-    # Check if the user input starts with "bookt:"
-    if user_input.startswith('bookt:'):
-        search_term = user_input[7:].strip()  # Extract the search term after "bookt:"
-        return get_bookt_response(search_term)
 
-    conn.close()
+    # Check if the user input matches a book title
+    book_match = re.match(r'^(\w+(\s\w+)*)$', user_input)
+    if book_match:
+        search_term = book_match.group(1)
+        return get_bookt_response(search_term)
+    
     return "I'm sorry, I didn't understand that."
 
 # Define Flask routes
